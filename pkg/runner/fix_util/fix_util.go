220d13cdd493241a515468163fedf80d3174050f
coffeemakingtoaster <coffeemakingtoaster@gmail.com>
2025-07-28 17:15:18 +0200

    feat(fix): extend fix utils

--- pkg/runner/fix_util/fix_util.go
+++ pkg/runner/fix_util/fix_util.go
@@ -1,12 +1,13 @@
 package fixutil
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
 	"github.com/coffeemakingtoaster/dockerfile-parser/pkg/ast"
-	"github.com/rs/zerolog/log"
 	"github.com/coffeemakingtoaster/whale-watcher/pkg/container"
+	"github.com/rs/zerolog/log"
 )
 
 type FixUtils struct {
@@ -32,16 +33,59 @@ func SetupFromContent(DockerfileContent []string) FixUtils {
 	return FixUtils{astRoot: root}
 }
 
-func (cu *FixUtils) AddRunInstruction(index int, command string) {
+// Find the last place where a run instruction contains the search string and append the command
+// Mainly meant for adding cleanup commands
+func (cu *FixUtils) AppendRunInstructionWithMatch(search, command string) bool {
+	layer_index := -1
+	instruction_index := -1
+	curr_index := 0
 	curr := cu.astRoot
-	ind := -1
-	for ind < index {
+	for curr != nil {
+		for instruction_count, instruction := range curr.Instructions {
+			switch run := instruction.(type) {
+			case *ast.RunInstructionNode:
+				for i := range run.Cmd {
+					if strings.Contains(run.Cmd[i], search) {
+						instruction_index = instruction_count
+						layer_index = curr_index
+					}
+				}
+			}
+		}
+		curr_index++
 		curr = curr.Subsequent
-		ind++
 	}
-	curr.Instructions = append(curr.Instructions, &ast.RunInstructionNode{
+	if layer_index == -1 {
+		return false
+	}
+
+	stage := cu.getStage(layer_index)
+
+	instr := stage.Instructions[instruction_index].(*ast.RunInstructionNode)
+	instr.Cmd = append(instr.Cmd, []string{"&&", command}...)
+
+	return true
+}
+
+func (cu *FixUtils) getStage(index int) *ast.StageNode {
+	curr := cu.astRoot
+	for index > 0 {
+		curr = curr.Subsequent
+		index--
+	}
+	return curr
+}
+
+func (cu *FixUtils) AddRunInstruction(command string) {
+	curr := cu.astRoot
+	for curr.Subsequent != nil {
+		curr = curr.Subsequent
+	}
+	last := curr.Instructions[len(curr.Instructions)-1]
+	curr.Instructions[len(curr.Instructions)-1] = &ast.RunInstructionNode{
 		Cmd: []string{command},
-	})
+	}
+	curr.Instructions = append(curr.Instructions, last)
 }
 
 func (cu *FixUtils) SetUser(user string) {
@@ -52,6 +96,15 @@ func (cu *FixUtils) SetUser(user string) {
 	last := curr.Instructions[len(curr.Instructions)-1]
 	curr.Instructions[len(curr.Instructions)-1] = &ast.UserInstructionNode{User: user}
 	curr.Instructions = append(curr.Instructions, last)
+}
+
+func (cu *FixUtils) CreateUser(user string) {
+	curr := cu.astRoot
+	for curr.Subsequent != nil {
+		curr = curr.Subsequent
+	}
+	cu.AddRunInstruction(fmt.Sprintf("groupadd -r %s && useradd --no-log-init -r -g %s %s", user, user, user))
+	cu.SetUser(user)
 }
 
 func (fu *FixUtils) Finish() {
