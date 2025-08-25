87774d8b5805f16639ae227e64247a3c86125c48
coffemakingtoaster <mh340@hdm-stuttgart.de>
2025-08-25 16:27:23 +0200

    feat(rules): update import statements

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -11,22 +11,33 @@ type Runner interface {
 	ToString() string
 }
 
+var targetScore = map[string]int{
+	"command": 0,
+	"fs":      1,
+	"os":      2,
+}
+
 func NewPythonRunner(target string) (Runner, error) {
 	runner := &PythonRunner{
 		exec:             "python3",
 		workingDirectory: GetReferencingWorkingDirectoryInstance(),
 	}
 	importTemplate := ""
-	switch target {
-	case "command":
-		importTemplate = "from command_util_build import commandutil; command_util = commandutil.setup_from_path('{{ .DockerfilePath }}')"
-	case "fs":
-		importTemplate = "from fs_util_build import fsutil; fs_util = fsutil.setup('{{ .OciImage }}')"
-	case "os":
-		importTemplate = "from os_util_build import osutil; os_util = osutil.setup('{{ .DockerImage }}')"
-	default:
+	score, ok := targetScore[target]
+	if !ok {
 		return nil, fmt.Errorf("Unsupported target: %s! Supported targets are: command, fs, os", target)
 	}
+
+	if score >= 0 {
+		importTemplate += "from command_util_build import commandutil; command_util = commandutil.setup_from_path('{{ .DockerfilePath }}');"
+	}
+	if score >= 1 {
+		importTemplate += "from fs_util_build import fsutil; fs_util = fsutil.setup('{{ .OciImage }}');"
+	}
+	if score >= 2 {
+		importTemplate += "from os_util_build import osutil; os_util = osutil.setup('{{ .DockerImage }}');"
+	}
+
 	var err error
 	runner.utilImport, err = template.New("").Parse(importTemplate)
 	if err != nil {
